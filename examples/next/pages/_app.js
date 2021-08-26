f69b8a28699235919909a92b370335502a2f612a
Ryan Rishi <rrishi@envoy.com>
2021-08-26 14:49:48 -0700

    add Polarwind

--- examples/next/pages/_app.js
+++ examples/next/pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import { AppProvider } from '@envoy/polarwind'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  )
 }
 
 export default MyApp
