8a85008de35d16b399c294b3ecd05810c0166ae2
Kamal Mahyuddin <kamal@envoy.com>
2021-11-01 15:48:57 -0700

    Add SSRProvider temporarily

--- examples/next.js/pages/_app.js
+++ examples/next.js/pages/_app.js
@@ -1,10 +1,14 @@
 import "@envoy/polarwind/polarwind.css";
 import { AppProvider } from "@envoy/polarwind";
+/** TODO: remove SSRProvider when @envoy/polarwind includes it by default in AppProvider */
+import { SSRProvider } from "@react-aria/ssr";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
-      <Component {...pageProps} />
+      <SSRProvider>
+        <Component {...pageProps} />
+      </SSRProvider>
     </AppProvider>
   );
 }
