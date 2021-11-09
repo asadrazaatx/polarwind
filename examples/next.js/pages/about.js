ca36d2333f86ccaa7983e337094eb14396f40555
Kamal Mahyuddin <kamal@envoy.com>
2021-11-09 11:52:46 -0800

    Add another page to show internal links

---
+++ examples/next.js/pages/about.js
@@ -0,0 +1,12 @@
+import { Link } from "@envoy/polarwind";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Go back <Link url="/">home</Link>
+      </p>
+    </div>
+  );
+}
